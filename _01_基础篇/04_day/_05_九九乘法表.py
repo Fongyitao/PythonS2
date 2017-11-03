c635d20ff5ce17e4b4c00d69e422ff6dfb86e56f
Fongyitao <2059634680@qq.com>
2017-11-03 20:54:38 +0800

    learn
    Signed-off-by: Fongyitao <2059634680@qq.com>

---
+++ _01_基础篇/04_day/_05_九九乘法表.py
@@ -0,0 +1,24 @@
+# _date_:2017/11/2 21:38
+
+def method_name():
+    i = 1
+    j = 1
+    while i <= 9:
+        while j <= 9:
+            print(i, "x", j, "= %d" % (i * j), end="\t")
+            j += 1
+        i += 1
+        j = i
+        print()
+
+
+#method_name()
+
+i = 1
+while i <= 9:
+    j = 1
+    while j <= i:
+        print(j, "x", i, "= %d" % (i * j), end="\t")
+        j += 1
+    i += 1
+    print()
